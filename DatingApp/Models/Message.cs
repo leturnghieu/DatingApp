f3d2540483233b9e0d759c7834a7f455edbd70c5
letrunghieu <letrunghieu265.ns@gmail.com>
2023-10-30 11:34:00 +0700

    init

---
+++ DatingApp/Models/Message.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DatingApp.Models
+{
+    [Table("Message")]
+    public class Message
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid MessageId { get; set; }
+        [ForeignKey("User")]
+        public Guid SenderId { get; set; }
+        [ForeignKey("User")]
+        public Guid ReceiverId { get; set; }
+        [Required]
+        public string Content { get; set; }
+        [Required]
+        public DateTime SentDate { get; set; }
+    }
+}
